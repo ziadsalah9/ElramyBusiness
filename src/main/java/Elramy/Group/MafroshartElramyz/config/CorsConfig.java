9393a4b02dfadcfa27a5d22dc6721c280ccaf89b
Ziad Salah <ziads5933@gmail.com>
2026-09-23 16:58:51 +0300

    corrs

--- src/main/java/Elramy/Group/MafroshartElramyz/config/CorsConfig.java
+++ src/main/java/Elramy/Group/MafroshartElramyz/config/CorsConfig.java
@@ -2,6 +2,8 @@ package Elramy.Group.MafroshartElramyz.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -12,15 +14,17 @@ import java.util.List;
 public class CorsConfig {
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE) // 👈 يضمن تنفيذ فحص CORS قبل أي سكيورتي أو JWT
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
+        configuration.setAllowCredentials(true);
         configuration.setAllowedOriginPatterns(List.of(
-//                "http://localhost:5173",
-//                "http://localhost:3000",
-//                "https://admin-buddy-62.lovable.app",
-//                "https://arabic-furniture-man-98a2.bolt.host"
+                "http://localhost:5173",
+             "http://localhost:3000",
+                "https://admin-buddy-62.lovable.app",
+                "https://arabic-furniture-man-98a2.bolt.host"
                 "*"
         ));
 
